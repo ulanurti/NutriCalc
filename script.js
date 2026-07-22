43d4176b11b418c0ae03884e526ba2a3f03c2e38
ulanurti <ulyanak55555@gmail.com>
2026-07-22 17:41:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let patient = {
+    name: "",
+    age: "",
+    height: 0,
+    consultations: []
+};
 
+function calculateBMI() {
+
+    let weight = Number(document.getElementById("weight").value);
+    let height = Number(document.getElementById("height").value);
+    let waist = Number(document.getElementById("waist").value);
+    let hips = Number(document.getElementById("hips").value);
+    let shoulders = Number(document.getElementById("shoulders").value);
+
+    height = height / 100;
+
+    let bmi = weight / (height * height);
+
+    patient.height = height;
+
+    patient.consultations.push({
+        date: new Date(),
+        weight: weight,
+        waist: waist,
+        hips: hips,
+        shoulders: shoulders,
+        bmi: bmi
+    });

942facaf8f47fca8cdd0ab98f2ae4643b22dbaee
ulanurti <ulyanak55555@gmail.com>
2026-08-04 18:55:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,246 +14,155 @@ let patient = {
 
 let nutritionRules = {
 
-
     healthy: {
-
         name: "Здоровый человек",
-
         weightType: "actual",
-
-        calories: [25, 30],      // ккал/кг
-
-        protein: [0.8, 1.0],     // г/кг
-
-        fatPercent: [25, 35]     // % калорийности
-
+        calories: [25, 30],
+        protein: [0.8, 1.0],
+        fatPercent: [25, 35]
     },
 
 
     obesity: {
-
         name: "Ожирение",
-
         weightType: "corrected",
-
         calories: [20, 25],
-
         protein: [1.2, 1.5],
-
         fatPercent: [25, 30]
-
     },
 
 
     ckd: {
-
         name: "Хроническая болезнь почек",
-
         weightType: "actual",
-
         calories: [30, 35],
-
         protein: [0.6, 0.8],
-
         fatPercent: [30, 35]
-
     },
 
 
     dialysis: {
-
         name: "Гемодиализ",
-
         weightType: "actual",
-
         calories: [30, 35],
-
         protein: [1.0, 1.2],
-
         fatPercent: [30, 35]
-
     },
 
 
     cirrhosis: {
-
         name: "Цирроз печени",
-
         weightType: "actual",
-
         calories: [30, 35],
-
         protein: [1.2, 1.5],
-
         fatPercent: [30, 35]
-
     },
 
 
     pregnancy: {
-
         name: "Беременность",
-
         weightType: "actual",
-
         calories: [30, 35],
-
         protein: [1.1, 1.3],
-
         fatPercent: [30, 35]
-
     },
 
 
     lactation: {
-
         name: "Грудное вскармливание",
-
         weightType: "actual",
-
         calories: [30, 35],
-
         protein: [1.1, 1.3],
-
         fatPercent: [30, 35]
-
     },
 
 
     anorexia: {
-
         name: "Анорексия",
-
         weightType: "actual",
-
         calories: [30, 40],
-
         protein: [1.2, 1.5],
-
         fatPercent: [30, 35]
-
     },
 
 
     copd: {
-
         name: "ХОБЛ",
-
         weightType: "actual",
-
         calories: [30, 35],
-
         protein: [1.2, 1.5],
-
         fatPercent: [35, 40]
-
     }
 
 };
 
 
 
-//========================================
-// Режим консультации
-//========================================
+// ===============================
+// Цели консультации
+// ===============================
 
 let consultationModes = {
 
-
     maintain: {
-
         name: "Поддержание массы тела",
-
-        calorieCoefficient: 1.0,
-
-        description: "Поддержание текущей массы тела"
-
+        calorieCoefficient: 1
     },
 
 
     weightLoss: {
-
-        name: "Снижение массы тела (-15% за 6 месяцев)",
-
-        calorieCoefficient: 0.8,
-
-        description: "Снижение калорийности примерно на 20% от EAR"
-
+        name: "Снижение веса",
+        calorieCoefficient: 0.8
     },
 
 
     weightGain: {
-
-        name: "Набор массы тела",
-
-        calorieCoefficient: 1.15,
-
-        description: "Постепенное увеличение массы тела"
-
+        name: "Набор массы",
+        calorieCoefficient: 1.15
     },
 
 
     muscleGain: {
-
-        name: "Набор мышечной массы",
-
-        calorieCoefficient: 1.10,
-
-        description: "Увеличение мышечной массы при тренировках"
-
-    },
-
-
-    preoperative: {
-
-        name: "Подготовка к операции",
-
-        calorieCoefficient: 1.0,
-
-        description: "Оптимизация нутритивного статуса перед операцией"
-
-    },
-
-
-    rehabilitation: {
-
-        name: "Реабилитация",
-
-        calorieCoefficient: 1.10,
-
-        description: "Восстановление после заболевания или операции"
-
+        name: "Набор мышц",
+        calorieCoefficient: 1.1
     }
 
 };
 
-// Главная кнопка "Рассчитать"
+
+
+// ===============================
+// Главная функция
+// ===============================
 
 function calculateAll() {
 
-    // Основные расчеты
+
     let bmiResult = calculateBMI();
+
     let idealWeightResult = calculateIdealWeight();
+
     let correctedWeightResult = calculateCorrectedWeight();
+
     let bmrResult = calculateBMR();
+
     let earResult = calculateEAR();
 
 
-    // Цель консультации
+
     let goal = "maintain";
 
 
-    // Категория пациента
     let patientCategory =
         document.getElementById("patientCategory").value;
 
 
-    // Калории
+
     let caloriesResult =
         calculateCalories(goal, patientCategory);
 
 
-    // КБЖУ
+
     let macros =
         calculateMacros(goal, patientCategory);
 
@@ -261,76 +170,88 @@ function calculateAll() {
 
     document.getElementById("result").innerHTML =
 
-        "<h3>Результаты консультации</h3>" +
+    "<h3>Результаты консультации</h3>" +
 
-        "<b>ИМТ:</b> " +
-        bmiResult.bmi.toFixed(1) +
-        "<br>" +
+    "ИМТ: " + bmiResult.bmi.toFixed(1) +
+    "<br>" +
 
-        "<b>Категория ИМТ:</b> " +
-        bmiResult.category +
-        "<br><br>" +
+       "<b>Категория:</b> " +
+    bmiResult.category +
+    "<br><br>" +
 
-        "<b>Идеальная масса:</b> " +
-        Math.round(idealWeightResult) +
-        " кг<br>" +
+    "<b>Идеальная масса:</b> " +
+    Math.round(idealWeightResult) +
+    " кг<br>" +
 
-        "<b>Скорректированная масса:</b> " +
-        Math.round(correctedWeightResult) +
-        " кг<br><br>" +
+    "<b>Скорректированная масса:</b> " +
+    Math.round(correctedWeightResult) +
+    " кг<br><br>" +
 
-        "<b>BMR:</b> " +
-        Math.round(bmrResult) +
-        " ккал/сут<br>" +
+    "<b>BMR:</b> " +
+    Math.round(bmrResult) +
+    " ккал/сут<br>" +
 
-        "<b>EAR:</b> " +
-        Math.round(earResult) +
-        " ккал/сут<br><br>" +
+    "<b>EAR:</b> " +
+    Math.round(earResult) +
+    " ккал/сут<br><br>" +
 
-        "<b>Калории:</b> " +
-        Math.round(caloriesResult.min) +
-        " - " +
-        Math.round(caloriesResult.max) +
-        " ккал<br><br>" +
+    "<b>Калории:</b> " +
+    Math.round(caloriesResult.min) +
+    " - " +
+    Math.round(caloriesResult.max) +
+    " ккал<br><br>" +
 
-        "<b>Белки:</b> " +
-        Math.round(macros.proteinMin) +
-        " - " +
-        Math.round(macros.proteinMax) +
-        " г<br>" +
+    "<b>Белки:</b> " +
+    Math.round(macros.proteinMin) +
+    " - " +
+    Math.round(macros.proteinMax) +
+    " г<br>" +
 
-        "<b>Жиры:</b> " +
-        Math.round(macros.fatMin) +
-        " - " +
-        Math.round(macros.fatMax) +
-        " г<br>" +
+    "<b>Жиры:</b> " +
+    Math.round(macros.fatMin) +
+    " - " +
+    Math.round(macros.fatMax) +
+    " г<br>" +
 
-        "<b>Углеводы:</b> " +
-        Math.round(macros.carbsMin) +
-        " - " +
-        Math.round(macros.carbsMax) +
-        " г";
+    "<b>Углеводы:</b> " +
+    Math.round(macros.carbsMin) +
+    " - " +
+    Math.round(macros.carbsMax) +
+    " г";
 
 }
 
 
 
+// ===============================
 // Расчёт ИМТ
+// ===============================
 
 function calculateBMI() {
 
-    let weight = Number(document.getElementById("weight").value);
 
-    // Рост вводится в сантиметрах
-    let heightCm = Number(document.getElementById("height").value);
+    let weight =
+    Number(document.getElementById("weight").value);
 
-    // Для ИМТ переводим в метры
+
+    let heightCm =
+    Number(document.getElementById("height").value);
+
+
     let heightM = heightCm / 100;
 
 
-    let waist = Number(document.getElementById("waist").value);
-    let hips = Number(document.getElementById("hips").value);
-    let shoulders = Number(document.getElementById("shoulders").value);
+    let waist =
+    Number(document.getElementById("waist").value);
+
+
+    let hips =
+    Number(document.getElementById("hips").value);
+
+
+    let shoulders =
+    Number(document.getElementById("shoulders").value);
+
 
 
     let bmi = weight / (heightM * heightM);
@@ -340,226 +261,295 @@ function calculateBMI() {
 
 
     if (bmi < 18.5) {
+
         category = "Недостаточный вес";
-    }
-    else if (bmi < 25) {
+
+    } else if (bmi < 25) {
+
         category = "Нормальный вес";
-    }
-    else if (bmi < 30) {
+
+    } else if (bmi < 30) {
+
         category = "Избыточный вес";
-    }
-    else if (bmi < 35) {
+
+    } else if (bmi < 35) {
+
         category = "Ожирение I степени";
-    }
-    else if (bmi < 40) {
+
+    } else if (bmi < 40) {
+
         category = "Ожирение II степени";
-    }
-    else {
+
+    } else {
+
         category = "Ожирение III степени";
+
     }
+
 
 
     patient.height = heightCm;
 
 
+
     patient.consultations.push({
 
         date: new Date(),
+
         weight: weight,
+
         height: heightCm,
+
         waist: waist,
+
         hips: hips,
+
         shoulders: shoulders,
+
         bmi: bmi
 
     });
 
 
+
     return {
 
         bmi: bmi,
+
         category: category
 
     };
+
 
 }
 
 
 
-// Расчёт основного обмена BMR
+
+// ===============================
+// Основной обмен BMR
+// ===============================
 
 function calculateBMR() {
 
-    let weight = Number(document.getElementById("weight").value);
 
-    // Здесь нужна высота в сантиметрах
-    let heightCm = Number(document.getElementById("height").value);
+    let weight =
+    Number(document.getElementById("weight").value);
 
-    let age = Number(document.getElementById("age").value);
 
-    let gender = document.getElementById("gender").value;
+    let heightCm =
+    Number(document.getElementById("height").value);
+
+
+    let age =
+    Number(document.getElementById("age").value);
+
+
+    let gender =
+    document.getElementById("gender").value;
+
 
 
     let bmr;
 
 
+
     if (gender === "male") {
 
-        bmr =
-            10 * weight +
-            6.25 * heightCm -
-            5 * age +
-            5;
-
-    }
-    else {
 
         bmr =
-            10 * weight +
-            6.25 * heightCm -
-            5 * age -
-            161;
+        10 * weight +
+        6.25 * heightCm -
+        5 * age +
+        5;
+
+
+    } else {
+
+
+        bmr =
+        10 * weight +
+        6.25 * heightCm -
+        5 * age -
+        161;
 
     }
 
 
-    document.getElementById("result").innerHTML =
-    "Основной обмен: " + Math.round(bmr) + " ккал";
 
+    return bmr;
+
+}
+
+
+
+// ===============================
+// Суточная потребность EAR
+// ===============================
 
 function calculateEAR() {
 
-    // Получаем основной обмен
+
     let bmr = calculateBMR();
 
-    // Получаем коэффициент активности
-    let activity = Number(document.getElementById("activity").value);
 
-    // Рассчитываем суточную потребность
+    let activity =
+    Number(document.getElementById("activity").value);
+
+
+
     let ear = bmr * activity;
 
-    // Выводим результат
-    document.getElementById("result").innerHTML +=
-        "<br>Основной обмен: " +
-        Math.round(bmr) +
-        " ккал" +
-        "<br>Суточная потребность (EAR): " +
-        Math.round(ear) +
-        " ккал";
 
     return ear;
+
 }
-// Расчёт идеального веса
+
+        
+/// ===============================
+// Идеальный вес
+// ===============================
 
 function calculateIdealWeight() {
 
 
-    // Рост берём в сантиметрах
-    let heightCm = Number(document.getElementById("height").value);
+    let heightCm =
+    Number(document.getElementById("height").value);
 
-    let gender = document.getElementById("gender").value;
+
+    let gender =
+    document.getElementById("gender").value;
+
 
 
     let brock;
+
     let devine;
+
 
 
     if (gender === "male") {
 
+
         brock = heightCm - 100;
 
-        devine =
-            50 + 0.9 * (heightCm - 152.4);
 
-    }
-    else {
+        devine =
+        50 + 0.9 * (heightCm - 152.4);
+
+
+
+    } else {
+
 
         brock = heightCm - 110;
 
+
         devine =
-            45.5 + 0.9 * (heightCm - 152.4);
+        45.5 + 0.9 * (heightCm - 152.4);
+
 
     }
 
 
-    // Для ИМТ нужен рост в метрах
 
-    let heightM = heightCm / 100;
-
-
-    let targetMin =
-        21 * heightM * heightM;
-
-    let targetMax =
-        23 * heightM * heightM;
-
-
-
-    document.getElementById("idealWeight").innerHTML =
-
-        "<b>Идеальный вес</b><br>" +
-        "По Броку: " + brock.toFixed(1) + " кг<br>" +
-        "По Девину: " + devine.toFixed(1) + " кг<br>" +
-        "Диапазон ИМТ 21-23: " +
-        targetMin.toFixed(1) +
-        " - " +
-        targetMax.toFixed(1) +
-        " кг";
+    return devine;
 
 }
-    function calculateCalories(goal, patientCategory) {
 
 
-    let rules = nutritionRules[patientCategory];
 
-    let mode = consultationModes[goal];
+// ===============================
+// Скорректированная масса тела
+// ===============================
 
-
-    let actualWeight = Number(
-        document.getElementById("weight").value
-    );
+function calculateCorrectedWeight() {
 
 
-    let correctedWeight = calculateCorrectedWeight();
+    let actualWeight =
+    Number(document.getElementById("weight").value);
 
 
-    // Выбираем массу для расчета
 
-    let weight;
+    let idealWeight =
+    calculateIdealWeight();
+
+
+
+    // Формула: идеальный + 0.4*(фактический - идеальный)
+
+    let corrected =
+    idealWeight +
+    0.4 * (actualWeight - idealWeight);
+
+
+
+    return corrected;
+
+}
+
+
+
+
+// ===============================
+// Расчёт калорий
+// ===============================
+
+function calculateCalories(goal, patientCategory) {
+
+
+    let rules =
+    nutritionRules[patientCategory];
+
+
+    let mode =
+    consultationModes[goal];
+
+
+
+    if (!rules) {
+
+        rules = nutritionRules.healthy;
+
+    }
+
+
+    if (!mode) {
+
+        mode = consultationModes.maintain;
+
+    }
+
+
+
+    let weight =
+    Number(document.getElementById("weight").value);
+
 
 
     if (rules.weightType === "corrected") {
 
-        weight = correctedWeight;
-
-    } 
-    else {
-
-        weight = actualWeight;
+        weight = calculateCorrectedWeight();
 
     }
 
 
 
-    // Диапазон калорий
-
     let minCalories =
-        rules.calories[0] * weight;
+    rules.calories[0] * weight;
 
 
     let maxCalories =
-        rules.calories[1] * weight;
+    rules.calories[1] * weight;
 
 
-
-    // Учитываем цель консультации
 
     minCalories =
-        minCalories * mode.calorieCoefficient;
+    minCalories * mode.calorieCoefficient;
 
 
     maxCalories =
-        maxCalories * mode.calorieCoefficient;
+    maxCalories * mode.calorieCoefficient;
 
 
 
@@ -571,93 +561,180 @@ function calculateIdealWeight() {
 
     };
 
+
 }
+
+
+
+
+
+// ===============================
+// КБЖУ
+// ===============================
 
 function calculateMacros(goal, patientCategory) {
 
-    let weight = Number(document.getElementById("weight").value);
 
-    if (!weight) {
-        return {
-            proteinMin: 0,
-            proteinMax: 0,
-            fatMin: 0,
-            fatMax: 0,
-            carbsMin: 0,
-            carbsMax: 0
-        };
-    }
+    let rules =
+    nutritionRules[patientCategory];
 
 
-    let calories = calculateCalories(goal, patientCategory);
+
+    let weight =
+    Number(document.getElementById("weight").value);
 
 
-    let proteinMin = weight * 1.2;
-    let proteinMax = weight * 1.5;
+
+    let calories =
+    calculateCalories(goal, patientCategory);
 
 
-    let fatMin = calories.min * 0.25 / 9;
-    let fatMax = calories.max * 0.35 / 9;
+
+    let proteinMin =
+    weight * rules.protein[0];
 
 
-    let carbsMin = 
-        (calories.min - proteinMin * 4 - fatMin * 9) / 4;
+    let proteinMax =
+    weight * rules.protein[1];
 
 
-    let carbsMax = 
-        (calories.max - proteinMax * 4 - fatMax * 9) / 4;
+
+    let fatMin =
+    calories.min *
+    rules.fatPercent[0] / 100 / 9;
+
+
+    let fatMax =
+    calories.max *
+    rules.fatPercent[1] / 100 / 9;
+
+
+
+
+    let carbsMin =
+    (calories.min -
+    proteinMin * 4 -
+    fatMin * 9) / 4;
+
+
+
+    let carbsMax =
+    (calories.max -
+    proteinMax * 4 -
+    fatMax * 9) / 4;
 
 
 
     return {
 
+
         proteinMin: proteinMin,
+
         proteinMax: proteinMax,
 
+
         fatMin: fatMin,
+
         fatMax: fatMax,
 
+
         carbsMin: carbsMin,
+
         carbsMax: carbsMax
+
 
     };
 
+
 }
+
+
+
+
+// ===============================
 // История консультаций
+// ===============================
 
 function showConsultation() {
+
 
     let text = "";
 
 
-    for (let i = 0; i < patient.consultations.length; i++) {
 
-        let c = patient.consultations[i];
+    for (
+        let i = 0;
+        i < patient.consultations.length;
+        i++
+    ) {
+
+
+        let c =
+        patient.consultations[i];
+
 
 
         text +=
 
-            "<b>Консультация " + (i + 1) + "</b><br>" +
-            "Вес: " + c.weight + " кг<br>" +
-            "Рост: " + c.height + " см<br>" +
-            "ИМТ: " + c.bmi.toFixed(1) + "<br>" +
-            "Талия: " + c.waist + " см<br>" +
-            "Бёдра: " + c.hips + " см<br>" +
-            "Плечо: " + c.shoulders + " см<br><br>";
+
+        "<b>Консультация " +
+        (i + 1) +
+        "</b><br>" +
+
+
+        "Вес: " +
+        c.weight +
+        " кг<br>" +
+
+
+        "Рост: " +
+        c.height +
+        " см<br>" +
+
+
+        "ИМТ: " +
+        c.bmi.toFixed(1) +
+        "<br>" +
+
+
+        "Талия: " +
+        c.waist +
+        " см<br>" +
+
+
+        "Бёдра: " +
+        c.hips +
+        " см<br>" +
+
+
+        "Плечо: " +
+        c.shoulders +
+        " см<br><br>";
 
     }
 
 
-    document.getElementById("history").innerHTML = text;
+
+    document.getElementById("history").innerHTML =
+    text;
+
 
 }
 
 
 
-// Пока заглушка
+
+
+// ===============================
+// График (пока заглушка)
+// ===============================
 
 function showGraph() {
 
-    alert("График добавим следующим этапом");
+
+    alert(
+    "График добавим следующим этапом"
+    );
+
 
 }
